a2e266a9f5514df4e514cd276010c4a9e373ffc9
Ibrahim Alassaf <xibra9141@gmail.com>
2022-08-04 01:28:29 +0300

    feat: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./views/*.{html,js,tsx,ejs}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+        content: ["./views/*.{html,js,tsx,ejs}"],
+        theme: {
+            extend: {},
+        },
+        plugins: [],
+    }
+    // make sure to run npm tailwind:css after every CSS change
